6541db3fb772a93e238f90b53b93ee61757eb960
Weliton Martins <weliton.martinsprofissional@gmail.com>
2018-08-22 22:06:05 -0300

    primeiro commit, com configurações basica do spring e H2, model Categoria e teste do Rest

---
+++ src/main/java/com/welitonmartins/LojaIonicApplication.java
@@ -0,0 +1,12 @@
+package com.welitonmartins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaIonicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaIonicApplication.class, args);
+	}
+}
---
+++ src/main/java/com/welitonmartins/resources/CategoriaResource.java
@@ -0,0 +1,27 @@
+package com.welitonmartins.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.welitonmartins.model.Categoria;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+	
+	@GetMapping
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "info");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		return lista;
+	}
+}
