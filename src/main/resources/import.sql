57b4c519278ab6999c01b01397301b02115c30db
weliton <weliton.martinsprofissional@gmail.com>
2018-09-23 16:21:23 -0300

    cliente, tipoCliente, endereco, enum

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -17,6 +17,17 @@ insert into estado(nome,)values('Sao Paulo');
 
 insert into cidade(nome,estado_id)values('Sao Paulo',2);
 insert into cidade(nome,estado_id)values('Campinas',2);
-insert into cidade(nome,estado_id)values('Uberlândia',1);
+insert into cidade(nome,estado_id)values('Uberlândia',1); 
+
+
+insert into cliente(cpf,email,nome,tipo)values('78522566985','email@gmail.com','Maria Silva',1); 
+
+insert into TELEFONE(cliente_id,telefones)values(1,'9999-5555');
+insert into TELEFONE(cliente_id,telefones)values(1,'9999-2584');
+
+insert into ENDERECO (bairro,cep,complemento,logradouro,numero,cidade_id,cliente_id)values('Jardim','6720-458','Quebrada','Rua das Flores','58',1,1); 
+insert into ENDERECO (bairro,cep,complemento,logradouro,numero,cidade_id,cliente_id)values('Jardim','6720-458','blalbalbala','Avenida','90',2,1); 
+
+
 
 
