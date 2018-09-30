46697858152e4ada8106f7a8271fdba9a3763f5a
Weliton Alves Martins <weliton.martins@yaman.com.br>
2018-09-30 15:19:44 -0300

    personalizando o retorno de validacao

--- src/main/java/com/welitonmartins/resources/execptions/ResourceExceptionHandler.java
+++ src/main/java/com/welitonmartins/resources/execptions/ResourceExceptionHandler.java
@@ -4,6 +4,8 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -26,11 +28,23 @@ public class ResourceExceptionHandler {
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(erro);
 	}
 	
-	@ExceptionHandler(DataIntegrityException.class)// aqui indica que é um tratador de exceção desse tipo de exceção ObjectNotFoundException
+	@ExceptionHandler(DataIntegrityException.class)// aqui indica que é um tratador de exceção desse tipo de exceção DataIntegrityException
 	public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request){
 		
 		StandardError erro = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());//System.currentTimeMillis pega a hora atual em milesegundo
 //linha a cima, instanciando a classes StandardError e passando seus valores(status, msg, e hora)
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erro);
+	}
+	//personalizando o erro de validacao 
+	@ExceptionHandler(MethodArgumentNotValidException.class)// aqui indica que é um tratador de exceção desse tipo de exceção MethodArgumentNotValidException
+	public ResponseEntity<StandardError> validation(MethodArgumentNotValidException e, HttpServletRequest request){
+		
+		ValidationError erro = new ValidationError(HttpStatus.BAD_REQUEST.value(),"Erro de validação", System.currentTimeMillis());//System.currentTimeMillis pega a hora atual em milesegundo
+		
+		for(FieldError x : e.getBindingResult().getFieldErrors()) {
+			erro.addError(x.getField(), x.getDefaultMessage());
+		}
 		
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(erro);
 	}
--- src/main/java/com/welitonmartins/resources/execptions/StandardError.java
+++ src/main/java/com/welitonmartins/resources/execptions/StandardError.java
@@ -1,9 +1,13 @@
 package com.welitonmartins.resources.execptions;
+
+import java.io.Serializable;
+
 /*
  * erro padrao
  * essa class vai auxiliar a ResourceExceptionHandler
  */
-public class StandardError {
+public class StandardError implements Serializable{
+	private static final long serialVersionUID = 1L;
 	
 	private Integer status;
 	private String msg;
