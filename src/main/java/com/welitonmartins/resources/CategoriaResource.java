81a15fc970a9ff76e0287254dcfb0f343a4359a0
Weliton Alves Martins <weliton.martins@yaman.com.br>
2018-09-29 21:37:47 -0300

    inserindo uma nova categoria com post

--- src/main/java/com/welitonmartins/resources/CategoriaResource.java
+++ src/main/java/com/welitonmartins/resources/CategoriaResource.java
@@ -1,13 +1,16 @@
 package com.welitonmartins.resources;
 
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.welitonmartins.model.Categoria;
 import com.welitonmartins.services.CategoriaService;
@@ -24,7 +27,15 @@ public class CategoriaResource {
 		Categoria obj = service.find(id);
 		//ResponseEntity tem varias informaçoes do protocolo http
 		//linha abaixo uma forma de falar que aconteceu tudo ok, e tem como corpo o "obj"
-		return ResponseEntity.ok().body(obj);
-		
+		return ResponseEntity.ok().body(obj);	
 	}
+		@RequestMapping(method=RequestMethod.POST)
+		public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+			obj = service.insert(obj); 
+			//pegando a requisição uri que vai ser inserida a nova informação
+			URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+					.path("/{id}").buildAndExpand(obj.getId()).toUri();
+			return ResponseEntity.created(uri).build();
+			//created(uri) -> gera o codigo 201
+		}
 }
